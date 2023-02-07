27e8c9bd8340ae1a721dd6781ae5c6dcf7242b1a
ukuz <70457027+ukuz@users.noreply.github.com>
2023-02-07 23:46:19 +0800

    Update index.js

--- pages/index.js
+++ pages/index.js
@@ -11,9 +11,9 @@ export default function Home() {
       </Head>
 
       <main>
-        <Header title="Welcome to my app!" />
+        <Header title="碎叶集" />
         <p className="description">
-          Get started by editing <code>pages/index.js</code>
+          of songs and culture.
         </p>
       </main>
 
