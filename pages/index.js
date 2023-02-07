453173a06b4f4c0aeaaa5f176880c25cfb75dab8
ukuz <70457027+ukuz@users.noreply.github.com>
2023-02-07 23:52:02 +0800

    Update index.js

--- pages/index.js
+++ pages/index.js
@@ -6,14 +6,14 @@ export default function Home() {
   return (
     <div className="container">
       <Head>
-        <title>Next.js Starter!</title>
+        <title>弓枚</title>
         <link rel="icon" href="/favicon.ico" />
       </Head>
 
       <main>
         <Header title="碎叶集" />
         <p className="description">
-          of songs and culture.
+          of song & culture.
         </p>
       </main>
 
